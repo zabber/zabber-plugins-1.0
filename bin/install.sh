c0c41ba2b4182b9830a5b76eef1dde640ed5b981
Dmitry Chernyak <dmi.chernyak@gmail.com>
2011-08-28 01:04:28 +0400

    Added general installation script

---
+++ bin/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+prefix=/usr/local/zabber-plugins
+agentconf=/etc/zabbix/zabbix_agentd.conf
+
+set -e
+wd=`pwd`
+cd ..
+mkdir -p $prefix
+
+cp -a $wd/* $prefix
+cd $prefix
+bin/zabbix-agent-install.sh
+bin/update-zabber-plugins
+
+echo Done.
+
+cat <<EOF
+
+You should place right config into $agentconf 
+
+You can get it from http://www.skycover.ru service.
+EOF
